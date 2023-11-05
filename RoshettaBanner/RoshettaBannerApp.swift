3f8a7427308e2ae71a59381df74afdab9e09ebda
Samiiahmed <eldon.smsm135@gmail.com>
2023-11-05 17:38:57 +0200

    Initial Commit

---
+++ RoshettaBanner/RoshettaBannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RoshettaBannerApp.swift
+//  RoshettaBanner
+//
+//  Created by Sami Ahmed on 05/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct RoshettaBannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
